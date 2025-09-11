55a006cf0f9f8bd330057e75d010bc689e1f7415
ZizhengLiu2719 <zl024@email.uark.edu>
2025-09-11 00:00:26 -0500

    修复加清理

--- check_travel_registrations_schema.sql
+++
@@ -1,8 +0,0 @@
--- 检查 travel_registrations 表的实际字段名
-\d travel_registrations;
-
--- 查看所有字段
-SELECT column_name, data_type, is_nullable, column_default
-FROM information_schema.columns 
-WHERE table_name = 'travel_registrations' 
-ORDER BY ordinal_position;
